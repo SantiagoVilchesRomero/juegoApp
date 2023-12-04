276f46b61dcb7800562f384eb129507c31ff017d
root <svilrom1212@ieszaidinvergeles.org>
2023-12-04 09:29:07 +0000

    proyecto Santi

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\JuegoController;
+use App\Http\Controllers\SettingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::resource('juego', JuegoController::class);
+
+Route::get('setting', [SettingController::class, 'index'])-> name('setting.index');
+Route::put('setting', [SettingController::class, 'update'])-> name('setting.update');
